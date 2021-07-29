1480b179a0680f35f7d0d9d70a98878eb802deb0
easbarbosa <easbarbosa@tutanota.me>
2021-07-29 00:48:17 -0300

    main: simpler passing of objects

--- lib/floss/main.rb
+++ lib/floss/main.rb
@@ -1,54 +1,58 @@
 # frozen_string_literal: true
 
-require 'pathname'
-
 module Floss
   # Core Management FLOSS Projects
   class Main
     attr_reader :folders, :utils, :command
 
     def initialize(services, command)
-      @folders = services.resolve :folders
       @utils = services.resolve :utils
       @command = command
     end
 
-    # a
+    # get all projects
     def parsed_projects
-      folder = folders.floss_config
-      utils.parse_folder folder
+      ParseProjects.new.parse_folder
     end
 
-    # a
+    # organize project information
     def process_projects
       parsed_projects.each do |language, projects|
         puts "\n❯ #{language.capitalize}"
         puts
 
-        projects.each do |url|
-          project_info = ProjectInfo.new url, language.to_s
+        projects.each do |project|
+          name = project[0].to_s
+          url = project[1]
+          project_info = ProjectInfo.new name, url, language.to_s
+
           yield project_info
         end
       end
     end
 
+    # TODO: isso aqui embaixo ta uma porra
+
     def archive
       process_projects do |info|
-        Archive.new(utils, info.name, info.folder, info.to_s).run
+        Archive.new(utils, info).run
       end
     end
+
     public :archive
 
     def grab
       process_projects do |info|
-        Grab.new(utils, info.folder, info.url, info.to_s).run
+        Grab.new(utils, info).run
       end
     end
+
     public :grab
 
     def run
       public_send(command)
     end
+
     public :run
   end
 end
