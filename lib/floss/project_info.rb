a52caa9c70e9f530a1158a15c8254028781b9cda
easbarbosa <easbarbosa@tutanota.me>
2021-07-29 00:47:51 -0300

    project_info: receive formatted info

--- lib/floss/project_info.rb
+++ lib/floss/project_info.rb
@@ -1,26 +1,25 @@
 # frozen_string_literal: true
 
 require 'pathname'
-require 'uri'
 
 module Floss
   # Provides Project Information
   class ProjectInfo
     attr_reader :url, :name, :folder
 
-    def initialize(url, language)
-      @url = URI.parse url
-      @language = language
-      @name = File.basename(@url.path.split('/').last, '.git')
-      projects = Pathname.new(File.join(Dir.home, 'Projects'))
-      @folder = projects.join language, name
+    def initialize(name, url, language)
+      @name = name
+      @url = url
+
+      root = Pathname.new(File.join(Dir.home, 'Projects'))
+      @folder = root.join language, name
     end
 
     def to_s
       <<~INFO
-          repository: #{url}
-          folder: #{folder}
-        INFO
+        repository: #{url}
+        folder: #{folder}
+      INFO
     end
   end
 end
