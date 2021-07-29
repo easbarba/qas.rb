1a4228ecdd38f39697cf4bd864846a30f50d8cfd
easbarbosa <easbarbosa@tutanota.me>
2021-07-28 23:31:37 -0300

    grab: expect object w/ all info

--- lib/floss/actions/grab.rb
+++ lib/floss/actions/grab.rb
@@ -5,31 +5,29 @@ require 'git'
 module Floss
   # Grab FLOSS Projects
   class Grab
-    attr_reader :utils, :folder, :url, :info
+    attr_reader :utils, :project
 
-    def initialize(utils, folder, url, info)
+    def initialize(utils, project)
       @utils = utils
-      @folder = folder
-      @url = url
-      @info = info
+      @project = project
     end
 
     def do_pull
       utils.spin('Pulling') do
-        repo = Git.open folder
+        repo = Git.open project.folder
         repo.pull 'origin', repo.current_branch
       end
     end
 
     def do_clone
       utils.spin('Cloning') do
-        Git.clone url, folder
+        Git.clone project.url, project.folder
       end
     end
 
     # Cloning/Pulling FLOSS Project
     def run
-      puts info.to_s
+      puts project.to_s
 
       if folder.exist?
         do_pull
