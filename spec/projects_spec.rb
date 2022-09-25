9a2b0ba19c24b9d86bff9f525529d5ed08d5c93f
easbarba <easbarba@outlook.com>
2022-09-25 01:51:34 -0300

    spec: fix projects

--- spec/projects_spec.rb
+++ spec/projects_spec.rb
@@ -5,22 +5,22 @@ require 'spec_helper'
 require 'qas'
 
 module Qas
-  RSpec.describe Config do
-    subject(:config) { Config.new }
+  RSpec.describe Projects do
+    subject(:projects) { Projects.new }
 
     context '#found' do
       it 'has, at least, one config file correctly parsed' do
-        expect(config.found).not_to be_nil
+        expect(projects.found).not_to be_nil
       end
 
       # it 'no config file was found' do
-      #   expect(config.found).not_to be_nil
+      #   expect(projects.found).not_to be_nil
       # end
     end
 
     context '#projects' do
       it "random project's URI attribute is a properly formatted" do
-        project = config.items.values.sample.first
+        project = projects.items.values.sample.first
         expect(project.url).to be_a_kind_of URI
       end
     end
